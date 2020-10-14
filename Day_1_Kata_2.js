ca75d23fb9943deb8afd1fe899824d7a620476d6
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-15 03:26:36 +0600

    all

--- Day_1_Kata_2.js
+++ Day_1_Kata_2.js
@@ -1,8 +1,9 @@
-const numbers = [19, 5, 5, 42, 2, 77];
+const numbers = [5, 8, 12, 19, 22];
 
 function sumTwoSmallestNumbers(numbers) {
   const twoLowestNumbers = numbers
-    .filter((number, i, src) => (src[i] < src[i + 1] ? number : null))
+    .sort((a, b) => (a > b ? 1 : -1))
+    .splice(0, 2)
     .reduce((cur, acc) => cur + acc);
 
   return twoLowestNumbers;
