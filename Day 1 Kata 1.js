93938590353c0d3b067d21d3fbbef174153c2f74
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-15 02:10:34 +0600

    all

--- Day 1 Kata 1.js
+++ Day 1 Kata 1.js
@@ -1,11 +1,11 @@
-const sample = ["a", "a", "b", "b"];
+const sample = ["A", "B", "C", "D", "A", "B"];
 
 var uniqueInOrder = function (iterable) {
   //your code here - remember iterable can be a string or an array
   const arr = [...iterable];
   const newArr = arr.filter((el, i, src) => {
-    if (i !== i + 1) {
-      return i;
+    if (src[i] !== src[i + 1] || (i === 0 && src[i] !== src[i + 1])) {
+      return el;
     }
   });
 
