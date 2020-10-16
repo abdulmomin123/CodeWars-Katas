b313fff4f87d597e7c013dc4569245ef0c1f2c74
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-17 00:54:30 +0600

    all

--- Kata_5.js
+++ Kata_5.js
@@ -2,6 +2,8 @@ const sample = [{ name: "Bart" }, { name: "Lisa" }, { name: "Maggie" }];
 const sampleTwo = [{ name: "Bart" }, { name: "Lisa" }];
 
 function list(names) {
+  if (names.length === 0) return "";
+
   let namesArr = names.map((el) => el.name);
   namesArr.splice(names.length - 1, 0, "&");
   return namesArr;
