dc5259c94a9c2a9e3dac74ae751a841995959179
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-16 08:41:19 +0600

    all

--- Kata_3.js
+++ Kata_3.js
@@ -1,4 +1,4 @@
-const sample = 1071225; // 45
+const sample = 135440716410000; // 45
 const sampleTwo = 91716553919377; // -1
 let guess = 1;
 let curGuess = guess;
@@ -18,6 +18,8 @@ function findNb(m) {
       if (result === m) {
         clearInterval(timer);
         return;
+      } else {
+        arr = [];
       }
     }
 
