a897e0408d3f69a737a20571b511b32560c40225
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-17 01:19:00 +0600

    all

--- Kata_5.js
+++ Kata_5.js
@@ -6,8 +6,10 @@ function list(names) {
   if (names.length < 2) return names[0].name;
 
   let namesArr = names.map((el) => el.name);
-  namesArr.splice(names.length - 1, 0, "&");
   namesArr = namesArr.join(", ");
+  // namesArr.splice(names.length - 1, 0, "&");
+  namesArr = [...namesArr];
+  namesArr.splice(namesArr.lastIndexOf(" "), 0, "& ");
   return namesArr;
 }
 const result = list(sample);
