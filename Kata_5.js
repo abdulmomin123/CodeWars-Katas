1652e147f3743ca3a929fcc7d0ed3ea47ead3ee4
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-17 00:25:55 +0600

    all

--- Kata_5.js
+++ Kata_5.js
@@ -1,3 +1,9 @@
+const sample = [{ name: "Bart" }, { name: "Lisa" }, { name: "Maggie" }];
+const sampleTwo = [{ name: "Bart" }, { name: "Lisa" }];
+
 function list(names) {
   //your code here
 }
+const result = list(sample);
+
+console.log(result);
