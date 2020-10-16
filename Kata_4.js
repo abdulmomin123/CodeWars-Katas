8d78e50593125c6c8bc9d52f2f2e6dd332d600cd
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-16 15:09:52 +0600

    all

--- Kata_4.js
+++ Kata_4.js
@@ -1,7 +1,11 @@
 const sample = "How can mirrors be real if our eyes aren't real";
 
 String.prototype.toJadenCase = function () {
-  //...
+  return this.split(" ").map((el) => {
+    return [...el].map((el, i) => (i === 0 ? el.toUpperCase() : el)).join("");
+  });
 };
 
 const result = sample.toJadenCase();
+
+console.log(result);
