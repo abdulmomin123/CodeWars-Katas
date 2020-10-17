9a6e3ec5d0b2000826c9feb370a81204c7d5ea91
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-18 02:07:17 +0600

    all

--- Kata_6.js
+++ Kata_6.js
@@ -1,3 +1,7 @@
+const ticket1 = [25, 25, 50]; // => YES
+const tickets2 = [25, 100]; // => NO. Vasya will not have enough money to give change to 100 dollars
+const tickets3 = [25, 25, 50, 50, 100]; // => NO. Vasya will not have the right bills to give 75 dollars of change (you can't make two bills of 25 from one of 50)
+
 function tickets(peopleInLine) {
   //   if (a) {
   //     return "YES";
@@ -5,3 +9,6 @@ function tickets(peopleInLine) {
   //     return "NO";
   //   }
 }
+const result = tickets(ticket1);
+
+console.log(result);
