42c2f1804ac9136ffff1bb4b85609738e90480aa
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-20 23:21:11 +0600

    all

--- Kata_6.js
+++ Kata_6.js
@@ -1,30 +1,30 @@
-// const ticket1 = [25, 25, 50]; // => YES
-// const tickets2 = [25, 100]; // => NO. Vasya will not have enough money to give change to 100 dollars
-// const tickets3 = [25, 25, 50, 50, 100]; // => NO. Vasya will not have the right bills to give 75 dollars of change (you can't make two bills of 25 from one of 50)
-// let avMoney = [];
+const ticket1 = [25, 25, 50]; // => YES
+const tickets2 = [25, 100]; // => NO. Vasya will not have enough money to give change to 100 dollars
+const tickets3 = [25, 25, 50, 50, 100]; // => NO. Vasya will not have the right bills to give 75 dollars of change (you can't make two bills of 25 from one of 50)
+let avMoney = [];
 
-// function tickets(peopleInLine) {
-//   peopleInLine.forEach((money) => {
-//     if (money <= 25) {
-//       avMoney.push(money);
-//     } else {
-//       const change = money - 25;
-//       const totalMoney = avMoney.reduce((cur, acc) => cur + acc);
+function tickets(peopleInLine) {
+  peopleInLine.forEach((money) => {
+    if (money <= 25) {
+      avMoney.push(money);
+    } else {
+      const change = money - 25;
+      const totalMoney = avMoney.reduce((cur, acc) => cur + acc);
 
-//       if (totalMoney >= change) {
-//         avMoney = [money];
-//       } else {
-//       }
-//     }
-//   });
+      if (totalMoney >= change) {
+        avMoney = [money];
+      } else {
+      }
+    }
+  });
 
-//   return avMoney;
-//   //   if (a) {
-//   //     return "YES";
-//   //   } else {
-//   //     return "NO";
-//   //   }
-// }
-// const result = tickets(ticket1);
+  return avMoney;
+  //   if (a) {
+  //     return "YES";
+  //   } else {
+  //     return "NO";
+  //   }
+}
+const result = tickets(ticket1);
 
-// console.log(result);
+console.log(result);
