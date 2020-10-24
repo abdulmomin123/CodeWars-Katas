0f6e8fa3e1faa6224ae44c2bb27c46dba3ce677f
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-25 00:43:18 +0600

    all

--- Kata_7.js
+++ Kata_7.js
@@ -1,3 +1,12 @@
+const [sampleOne, sampleTwo] = [
+  [false, 1, 0, 1, 2, 0, 1, 3, 'a'],
+  [1, 2, 0, 1, 0, 1, 0, 3, 0, 1],
+];
+
 const moveZeros = function (arr) {
   // TODO: Program me
 };
+
+const result = moveZeros(sampleOne);
+
+console.log(sampleOne);
