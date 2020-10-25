c64b5eb188fec6ca5d4af30c1b1c83aa1566a81d
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-26 02:38:51 +0600

    all

--- Kata_10.js
+++ Kata_10.js
@@ -5,8 +5,16 @@ logical value true or false depending on if the integer is a prime.
 
 function isPrime(num) {
   if (num < 2) return false;
+
+  let result = false;
+
+  for (i = 2; i < 11; i++) {
+    num % i === 0 ? (result = true) : (result = false);
+  }
+
+  return result;
 }
 
-const result = isPrime(2);
+const result = isPrime(10);
 
 console.log(result);
