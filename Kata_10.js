6b1c32657da354bf450b2f5bad95e73b1b504d71
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-26 02:12:00 +0600

    all

--- Kata_10.js
+++ Kata_10.js
@@ -4,5 +4,9 @@ logical value true or false depending on if the integer is a prime.
 */
 
 function isPrime(num) {
-  //TODO
+  if (num < 2) return false;
 }
+
+const result = isPrime(2);
+
+console.log(result);
