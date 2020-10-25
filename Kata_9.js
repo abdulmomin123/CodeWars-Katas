266375b1cbf1154584347cd752db4a2672d2d8f6
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-26 01:11:47 +0600

    all

---
+++ Kata_9.js
@@ -0,0 +1,15 @@
+/*
+What is an anagram? Well, two words are anagrams of each other 
+if they both contain the same letters. For example:
+*/
+
+const word = 'abba',
+  words = ['aabb', 'abcd', 'bbaa', 'dada'];
+
+function anagrams(word, words) {
+  //
+}
+
+const result = anagrams(word, words);
+
+console.log(result);
