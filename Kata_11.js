29021e9507640d24b6bb3b0fe95a7a7837efe667
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-26 16:58:49 +0600

    all

--- Kata_11.js
+++ Kata_11.js
@@ -18,7 +18,8 @@ function likes(names) {
 
   if (names.length === 2) return `${names[0]} and ${names[1]} like this`;
 
-  if (names.length === 3) return 3;
+  if (names.length === 3)
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
 }
 
 const result = likes(samples[2]);
