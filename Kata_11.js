748cd67dc802ed9fe95db209f1a7bacdee4d87b3
Abdul Momin <abdulmomin114477@gmail.com>
2020-10-26 16:51:12 +0600

    all

---
+++ Kata_11.js
@@ -0,0 +1,20 @@
+/*
+Implement a function likes :: [String] -> String, which must take in 
+input array, containing the names of people who like an item. It must 
+return the display text as shown in the examples:
+*/
+
+const samples = [
+  ['Peter'],
+  ['Jacob', 'Alex'],
+  ['Max', 'John', 'Mark'],
+  ['Alex', 'Jacob', 'Mark', 'Max'],
+];
+
+function likes(names) {
+  // TODO
+}
+
+const result = likes(samples[0]);
+
+console.log(result);
